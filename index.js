e5cf20d965c62fde62fb4473a94d08240ad618ba
Amir Abu Shareb <yields@icloud.com>
2013-10-01 18:42:51 +0300

    use yields/approximate-time, closes #1

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 
 /**
+ * dependencies
+ */
+
+var time = require('approximate-time');
+
+/**
  * Convert the given `Date` to a human readable string.
  *
  * @param {Date} date
@@ -8,53 +14,8 @@
  */
 
 module.exports = function(date){
-  var now = new Date
-    , diff = now - date
-    , t;
-
-  // Ignore future.
-  if (date > now) return '';
-
-  // just now
-  if (1e3 > diff) return 'just now';
-
-  // seconds
-  t = diff / 1e3 | 0;
-  if (60 > t) return format(t, 'second');
-
-  // minutes
-  t = diff / 6e4 | 0;
-  if (60 > t) return format(t, 'minute');
-
-  // hours
-  t = diff / 3.6e+6 | 0;
-  if (24 > t) return format(t, 'hour');
-
-  // days
-  t = diff / 8.64e+7;
-  if (30.4368 > t) return format(t | 0, 'day');
-
-  // months
-  t = diff / 2.63e+9 | 0;
-  if (12 > t) return format(t, 'month');
-
-  // years
-  t = diff / 3.156e+10 | 0;
-  return format(t, 'year');
+  if (new Date < date) return '';
+  var ret = time(date);
+  if ('just now' == ret) return ret;
+  return time(date) + ' ago';
 };
-
-/**
- * Format the given `n` with `unit`.
- *
- * @param {Number} n
- * @param {String} unit
- * @return {String}
- * @api private
- */
-
-function format(n, unit){
-  var a = 'hour' == unit ? 'an' : 'a';
-  return (1 == n ? a : n)
-    + ' ' + (1 == n ? unit : unit + 's')
-    + ' ago';
-}
